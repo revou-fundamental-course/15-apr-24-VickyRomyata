6a0be9ebf341d55795292be03c42388c0c1eb016
VickyUlfaRomyataSembiring <38787923+VickyRomyata@users.noreply.github.com>
2024-04-24 21:17:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function convert() {
-    var temperatureInput = parseFloat(document.getElementById('temperatureInput').value);
-    var resultOutput = document.getElementById('resultOutput');
-    var calculationExplanation = document.getElementById('calculationExplanation');
+    let temperatureInput = parseFloat(document.getElementById('temperatureInput').value);
+    let resultOutput = document.getElementById('resultOutput');
+    let calculationExplanation = document.getElementById('calculationExplanation');
 
     if (!isNaN(temperatureInput)) {
-        var fahrenheit = (temperatureInput * 9 / 5) + 32;
+        let fahrenheit = (temperatureInput * 9 / 5) + 32;
         resultOutput.value = fahrenheit.toFixed(2) + ' °F';
-        calculationExplanation.value = temperatureInput + ' °C * (9/5) + 32 = ' + fahrenheit.toFixed(2) + ' °F';
+        calculationExplanation.value = temperatureInput + ' °C * 9/5 + 32 = ' + fahrenheit.toFixed(2) + ' °F';
     } else {
         resultOutput.value = 'Harap masukkan nilai!';
         calculationExplanation.value = '';
@@ -20,13 +20,13 @@ function reset() {
 }
 
 function reverseConversion() {
-    var resultOutput = document.getElementById('resultOutput');
-    var temperatureInput = parseFloat(resultOutput.value);
+    let resultOutput = document.getElementById('resultOutput');
+    let temperatureInput = parseFloat(resultOutput.value);
 
     if (!isNaN(temperatureInput)) {
         var celsius = (temperatureInput - 32) * 5 / 9;
         document.getElementById('temperatureInput').value = celsius.toFixed(2);
-        document.getElementById('calculationExplanation').value = temperatureInput + ' °F - 32 * (5/9) = ' + celsius.toFixed(2) + ' °C';
+        document.getElementById('calculationExplanation').value = temperatureInput + ' °F - 32 * 5/9 = ' + celsius.toFixed(2) + ' °C';
     } else {
         document.getElementById('temperatureInput').value = 'Harap masukkan nilai!';
         document.getElementById('calculationExplanation').value = '';
